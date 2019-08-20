5dfd8fac45eae5d9acb29747151c0655e3fa3241
parvathi003 <51682284+parvathi003@users.noreply.github.com>
2019-08-20 14:32:53 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        
+       Console.WriteLine("enter the number ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int row = 1; row <= n; ++row)
+        {
+            for (int col = 1; col <= row; ++col)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
